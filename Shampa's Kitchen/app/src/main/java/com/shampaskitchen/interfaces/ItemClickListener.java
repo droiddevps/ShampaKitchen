957c45c4cec4778606c774e0d4cd32cbed923f93
droiddevps <droiddevps@gmail.com>
2018-12-10 20:18:03 +0530

    Cleared

--- Shampa's Kitchen/app/src/main/java/com/shampaskitchen/interfaces/ItemClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.shampaskitchen.interfaces;
-
-/**
- * Created by sayantan on 3/3/18.
- */
-
-public interface ItemClickListener {
-    void onItemClick(int position);
-}
--- Shampa's Kitchen/app/src/main/java/com/shampaskitchen/interfaces/UploadCallBack.java
+++
@@ -1,9 +0,0 @@
-package com.shampaskitchen.interfaces;
-
-/**
- * Created by sayantan on 7/2/18.
- */
-
-public interface UploadCallBack {
-    void onResponse(String s);
-}
